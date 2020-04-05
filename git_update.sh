1eee7f5e38c8c25fd7c454cd16c9a3a1a991198e
zhulu <shaonianzhentan@sina.cn>
2020-04-05 14:27:56 +0800

    一键拉取指定

---
+++ git_update.sh
@@ -0,0 +1,17 @@
+
+# 创建文件夹
+rm -rf ~/git_$DOMAIN
+mkdir ~/git_$DOMAIN
+cd ~/git_$DOMAIN
+# 拉取新文件
+git init
+git remote add -f origin https://github.com/shaonianzhentan/$DOMAIN
+git config core.sparsecheckout true
+echo "custom_components/$DOMAIN" >> .git/info/sparse-checkout
+cat .git/info/sparse-checkout
+git pull origin master
+# 更新文件
+cp -rf ~/git_$DOMAIN/custom_components/$DOMAIN /config/custom_components/$DOMAIN
+# 删除临时文件
+rm -rf ~/git_$DOMAIN
+
