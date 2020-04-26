ac9cab4a92c6559418bfe6a4fc1842f0b94110e9
zhulu <shaonianzhentan@sina.cn>
2020-04-26 19:58:45 +0800

    更新

--- git_update.sh
+++ git_update.sh
@@ -1,17 +1,15 @@
-
 # 创建文件夹
-rm -rf ~/git_$DOMAIN
-mkdir ~/git_$DOMAIN
-cd ~/git_$DOMAIN
+rm -rf ~/git_ha_file_explorer
+mkdir ~/git_ha_file_explorer
+cd ~/git_ha_file_explorer
 # 拉取新文件
 git init
-git remote add -f origin https://github.com/shaonianzhentan/$DOMAIN
+git remote add -f origin https://github.com.cnpmjs.org/shaonianzhentan/ha_file_explorer
 git config core.sparsecheckout true
-echo "custom_components/$DOMAIN" >> .git/info/sparse-checkout
+echo "custom_components/ha_file_explorer" >> .git/info/sparse-checkout
 cat .git/info/sparse-checkout
 git pull origin master
 # 更新文件
-cp -rf ~/git_$DOMAIN/custom_components/$DOMAIN /config/custom_components/$DOMAIN
+cp -rf ~/git_ha_file_explorer/custom_components/ha_file_explorer /root/.homeassistant/custom_components
 # 删除临时文件
-rm -rf ~/git_$DOMAIN
-
+rm -rf ~/git_ha_file_explorer
