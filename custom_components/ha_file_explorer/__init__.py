eaf6bd2afe5bb93b0730fcd54977f9e007a50c7c
zhulu <shaonianzhentan@sina.cn>
2020-09-11 16:34:48 +0800

    发布一个版本

--- custom_components/ha_file_explorer/__init__.py
+++ custom_components/ha_file_explorer/__init__.py
@@ -8,7 +8,7 @@ from .api import FileExplorer
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = 'ha_file_explorer'
-VERSION = '2.2.6'
+VERSION = '2.2'
 URL = '/' + DOMAIN +'-api-' + VERSION
 ROOT_PATH = '/' + DOMAIN +'-local/' + VERSION
 
