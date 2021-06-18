039a3d0e5c12b7f0521e9e6d0fd39fcbc23226d6
zhulu <zhululu@weihaizixun.com>
2021-06-18 17:53:59 +0800

    使用cdn解决加载速度慢的问题

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
-import '@mdi/font/css/materialdesignicons.min.css'
-import "@fontsource/roboto"
+// import '@mdi/font/css/materialdesignicons.min.css'
+// import "@fontsource/roboto"
 
 Vue.use(Vuetify);
 
