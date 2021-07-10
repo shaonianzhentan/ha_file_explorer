823c8c4c5ec0fc7d4481d16a1da6856abadeaa08
zhulu <shaonianzhentan@sina.cn>
2021-07-10 23:06:40 +0800

    update

--- custom_components/ha_file_explorer/shaonianzhentan.py
+++ custom_components/ha_file_explorer/shaonianzhentan.py
@@ -275,7 +275,8 @@ async def fetch_json(url):
 
 # 获取HTTP请求信息
 async def fetch_info(url):
-    async with aiohttp.ClientSession() as session:
+    headers = {'referer': url}
+    async with aiohttp.ClientSession(headers=headers) as session:
         async with session.get(url) as response:
             return {
               'status': response.status,
