67addcb1182167b13b47be13f4fd7972eba273d2
zhulu <shaonianzhentan@sina.cn>
2021-07-11 20:54:18 +0800

    up

--- custom_components/ha_file_explorer/test.py
+++ custom_components/ha_file_explorer/test.py
@@ -2,7 +2,7 @@ from shaonianzhentan import fetch_info, async_create_task
 
 
 async def test():
-    res = await fetch_info('https://music.163.com/song/media/outer/url?id=1841521017')
+    res = await fetch_info('https://music.163.com/song/media/outer/url?id=191248')
     print(res)
 
 async_create_task(test())
