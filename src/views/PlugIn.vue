222f2311501c42303fe35b7f083bf4943ed3ad81
zhulu <zhululu@weihaizixun.com>
2021-09-01 15:00:12 +0800

    使用远端配置

--- src/views/PlugIn.vue
+++ src/views/PlugIn.vue
@@ -158,114 +158,17 @@ import { mapActions } from "vuex";
 export default {
   data() {
     return {
-      mylist: [
-        {
-          domain: "ha_cloud_music",
-          name: "网易云音乐",
-          url: "https://github.com/shaonianzhentan/ha_cloud_music"
-        },
-        {
-          domain: "conversation",
-          name: "语音小助手",
-          url: "https://github.com/shaonianzhentan/conversation"
-        },
-        {
-          domain: "ha_baidu_map",
-          name: "百度地图",
-          url: "https://github.com/shaonianzhentan/ha_baidu_map"
-        },
-        {
-          domain: "ha_sidebar",
-          name: "侧边栏管理",
-          url: "https://github.com/shaonianzhentan/ha_sidebar"
-        },
-        {
-          domain: "ha_file_explorer",
-          name: "文件管理",
-          url: "https://github.com/shaonianzhentan/ha_file_explorer"
-        },
-        {
-          domain: "ha_qqmail",
-          name: "QQ邮箱通知",
-          url: "https://github.com/shaonianzhentan/ha_qqmail"
-        },
-        {
-          domain: "xiaomi_tv",
-          name: "小米电视",
-          url: "https://github.com/shaonianzhentan/xiaomi_tv"
-        },
-        {
-          domain: "hf_weather",
-          name: "和风天气",
-          url: "https://github.com/shaonianzhentan/hf_weather"
-        },
-        {
-          domain: "mipad_android",
-          name: "小米平板",
-          url: "https://github.com/shaonianzhentan/MiPadAndroid"
-        },
-        {
-          domain: "xiaomi_radio",
-          name: "小米电台",
-          url: "https://github.com/shaonianzhentan/xiaomi_radio"
-        },
-        {
-          domain: "ha_status_notification",
-          name: "状态提醒",
-          url: "https://github.com/shaonianzhentan/ha_status_notification"
-        },
-        {
-          domain: "meizu_ble",
-          name: "魅族智能遥控器",
-          url: "https://github.com/shaonianzhentan/meizu_ble"
-        }
-      ],
-      list: [
-        {
-          domain: "sonoff",
-          name: "SonoffLAN - 易微联",
-          url: "https://github.com/AlexxIT/SonoffLAN"
-        },
-        {
-          domain: "xiaomi_miio_airconditioningcompanion",
-          name: "小米空调伴侣",
-          url: "https://github.com/syssi/xiaomi_airconditioningcompanion"
-        },
-        {
-          domain: "hacs",
-          name: "HACS - 插件商店",
-          url: "https://github.com/hacs/integration"
-        },
-        {
-          domain: "havcs",
-          name: "HAVCS - 智能音箱服务",
-          url: "https://github.com/cnk700i/havcs"
-        },
-        {
-          domain: "nodered",
-          name: "hass-node-red",
-          url: "https://github.com/zachowj/hass-node-red"
-        },
-        {
-          domain: "ble_monitor",
-          name: "米家蓝牙设备监听",
-          url: "https://github.com/custom-components/ble_monitor"
-        },
-        {
-          domain: "xiaomi_miot",
-          name: "Xiaomi Miot - 小米云端设备",
-          url: "https://github.com/al-one/hass-xiaomi-miot"
-        }
-      ],
+      mylist: [],
+      list: [],
       pull: {
         domain: "",
-        url: ""
+        url: "",
       },
       items: [
         { title: "github.com.cnpmjs.org" },
         { title: "hub.fastgit.org" },
-        { title: "github.com" }
-      ]
+        { title: "github.com" },
+      ],
     };
   },
   methods: {
@@ -281,8 +184,8 @@ export default {
       this.fetchApi({
         type: "update",
         domain,
-        url: url.replace("https://github.com/", `https://${gitUrl}/`)
-      }).then(res => {
+        url: url.replace("https://github.com/", `https://${gitUrl}/`),
+      }).then((res) => {
         if (res.code === 0) {
           this.$toast(res.msg);
         }
@@ -292,13 +195,13 @@ export default {
       if (top.confirm("会修改系统里相关文件配置，不懂千万别确定")) {
         this.fetchApi({
           type: "update-source",
-          url
-        }).then(res => {
+          url,
+        }).then((res) => {
           this.$toast(res.msg);
         });
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
